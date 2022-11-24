2dcf518d87f95f4713d9220817462c8e9d992a02
Muhammad Uvais <muhammaduvais07@gmail.com>
2022-11-24 19:35:30 +0530

    Code 24-Nov-2022

---
+++ file5.py
@@ -0,0 +1,29 @@
+print("Hello World!")
+name = "Arwaz"
+print(name)
+a = 10
+b = 20
+c = 30
+print(a, b ,c )
+name2 = "Amaan"
+print(name, name2)
+print( "You name is", name )
+print("You name is " + name2 )
+age  = 20
+print("You age is", age)
+print("You age is "+ str(age ) )
+
+#print("You age is "+ age )
+
+print(name, name2)
+print( name +" " + name2)
+
+print("Hello" , end = ' ')
+print("User")
+
+#print( "Hello", name, "Welcome to the course"  )
+print( f"Hello {name} Welcome to the course "  )
+
+print( f"Hello {name} You age is {age} and welcome  ")
+
+
---
+++ file6.py
@@ -0,0 +1,14 @@
+#Input
+
+#name = input("Please enter your name")
+
+#print(f"Your name is {name}")
+
+print("==================================================")
+print()
+x = int( input("Please enter x: ") )
+y = int( input("Please enter y: ") )
+print("x=",type(x))
+print("y=",type(y))
+z = x + y
+print("z=", z)
