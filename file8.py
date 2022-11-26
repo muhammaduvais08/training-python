adbab0bea4e6a6ebe046e445af3023c1a4e97cd0
Muhammad Uvais <muhammaduvais07@gmail.com>
2022-11-26 19:23:33 +0530

    Class 26-Nov-2022

---
+++ file8.py
@@ -0,0 +1,16 @@
+x = 100
+y = 100.50
+y =  10
+
+z = 5 + 2j
+
+#print(type(z))
+
+#print( 10 < 5)
+
+x = False
+y = True
+#print(x)
+
+x = bool(0)
+print(x )
---
+++ file9.py
@@ -0,0 +1,44 @@
+#Python String
+
+name = 'New Delhi'
+name2 = "Mumbai"
+
+#print( type(name2))
+
+str = """ Streen Number 78
+Mumbai 110023
+India
+"""
+#print(str)
+
+name = 'New  Delhi'
+
+#print(name[0])
+
+#print(name[4])
+
+str = "Raj Shree Institude"
+
+#print(  str [-12])
+#print( len(str) )
+
+x = 'ree In'
+#print( x  in str)
+print( x  not in str)
+
+
+#slicing
+
+#print( str[4:9] )
+
+#print( str[4:15] )
+#print( str[6:12] )
+#print( str[4:])
+#print( str[:15] )
+#print( str[2:-4] )
+
+#print( str[-10:-4] )
+#print( str[-10:] )
+#print( str[:-5] )
+#print(str[-10: -15])
+#print(str[::-1])
